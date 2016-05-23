21a315fcf4c0e82419f798c57c3169f4f964ece5
Alejandro <AlexMJHS@gmail.com>
2016-05-23 08:50:39 -0600

    Add to repo

---
+++ src/ctec/controller/AbstractRunner.java
@@ -0,0 +1,14 @@
+package ctec.controller;
+
+import ctec.controller.*;
+
+public class AbstractRunner
+{
+	public AbstractController baseController;
+	
+	public static void main(String [] args)
+	{
+		AbstractController baseController = new AbstractController();
+		baseController.start();
+	}
+}
